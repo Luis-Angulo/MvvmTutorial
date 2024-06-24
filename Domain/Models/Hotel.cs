32f43fe83b8ba45a2bbea61d0c4a3170c9b209e4
LAngulo <luisf117@gmail.com>
2024-06-23 20:35:38 -0700

    Initial commit

---
+++ Domain/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models
+{
+    public class Hotel
+    {
+        private readonly ReservationBook _ReservationBook;
+        public string Name { get; }
+        public Hotel(string name)
+        {
+            _ReservationBook = new();
+            Name = name;
+        }
+    }
+}
