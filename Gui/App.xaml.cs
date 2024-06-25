74f13d52e407107945c1b4528c537dcbabf1a3bf
LAngulo <luisf117@gmail.com>
2024-06-25 00:27:38 -0700

    MakeReservationView 1st draft

--- Gui/App.xaml.cs
+++ Gui/App.xaml.cs
@@ -1,14 +1,37 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Domain.Models;
+
 using System.Windows;
 
 namespace Gui
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //var hotel = new Hotel("Casa Marco");
+            //try
+            //{
+            //    hotel.MakeReservation(new Reservation(
+            //        "SingletonSean"
+            //        ,new RoomId(1,3)
+            //        ,new DateTime(2000, 1, 1)
+            //        ,new DateTime(2000, 1, 2)
+            //    ));
+            //    hotel.MakeReservation(new Reservation(
+            //        "SingletonSean"
+            //        , new RoomId(1, 3)
+            //        , new DateTime(2000, 1, 1)
+            //        , new DateTime(2000, 1, 4)
+            //    ));
+            //} catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //var reservations = hotel.GetReservationsByUserName("SingletonSean");
+
+            base.OnStartup(e);
+        }
     }
 
 }
