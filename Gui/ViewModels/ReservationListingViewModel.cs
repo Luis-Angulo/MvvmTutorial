07b44a1638ee3c77fb46fdd43ad6eb5f9f31fefc
LAngulo <luisf117@gmail.com>
2024-06-27 14:00:23 -0700

    ReservationListingViewModel impl

--- Gui/ViewModels/ReservationListingViewModel.cs
+++ Gui/ViewModels/ReservationListingViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using Gui.ViewModels.Abstractions;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Gui.ViewModels
 {
     public class ReservationListingViewModel : ViewModelBase
     {
-
+        private ObservableCollection<ReservationViewModel> _Reservations;
+        public IEnumerable<ReservationViewModel> Reservations => _Reservations;
+        public ICommand MakeReservationCommand { get; }
+        public ReservationListingViewModel()
+        {
+            _Reservations = new ObservableCollection<ReservationViewModel>();
+            // TODO: Remove Test Data
+            _Reservations.Add(new ReservationViewModel(new("Luis", new(12, 24), DateTime.Now, DateTime.Now)));
+            _Reservations.Add(new ReservationViewModel(new("Paco", new(12, 32), DateTime.Now, DateTime.Now)));
+            _Reservations.Add(new ReservationViewModel(new("Pablo", new(12, 16), DateTime.Now, DateTime.Now)));
+        }
     }
 }
---
+++ Gui/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Gui.ViewModels.Abstractions;
+
+namespace Gui.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        private Reservation _Reservation;
+        public string UserName => _Reservation.UserName;
+        public string RoomId => _Reservation.RoomId.ToString();
+        public DateTime StartTime => _Reservation.StartTime;
+        public DateTime EndTime => _Reservation.EndTime;
+
+        public ReservationViewModel(Reservation r)
+        {
+            _Reservation = r;
+        }
+    }
+}
