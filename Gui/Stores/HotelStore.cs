a5c3a23e5db60bb39afaa89a7304b44ed568599d
LAngulo <luisf117@gmail.com>
2024-07-07 14:49:37 -0700

    Lazy loading Reservations

--- Gui/Stores/HotelStore.cs
+++ Gui/Stores/HotelStore.cs
@@ -7,13 +7,19 @@ namespace Gui.Stores
     {
         private readonly List<Reservation> _reservations;
         private readonly Hotel _hotel;
+        private readonly Lazy<Task> _initializeLazy;
         public IEnumerable<Reservation> Reservations => _reservations;
         public HotelStore(Hotel hotel)
         {
             _reservations = new List<Reservation>();
+            _initializeLazy = new Lazy<Task>(Initialize);
             _hotel = hotel;
         }
         public async Task Load()
+        {
+            await _initializeLazy.Value;
+        }
+        private async Task Initialize()
         {
             IEnumerable<Reservation> reservations = await _hotel.GetAllReservations();
 
