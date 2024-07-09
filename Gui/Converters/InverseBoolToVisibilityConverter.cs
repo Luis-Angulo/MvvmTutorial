95bf22dc745cb4cae727f3cbb96e9619ce5b4edd
LAngulo <luisf117@gmail.com>
2024-07-08 17:16:31 -0700

    custom converter, hide ResListView while loading, new Grid

---
+++ Gui/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Gui.Converters
+{
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        // Converts true to Visibility.Collapsed and false to Visibility.Visible
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {   
+            // TODO: Is this casting or testing for true/false? What happens when the value is false?
+            return value is bool boolValue && boolValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
