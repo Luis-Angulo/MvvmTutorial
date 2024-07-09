260afa15e63f00c34308894b149324549ce7942e
LAngulo <luisf117@gmail.com>
2024-07-08 18:55:01 -0700

    Added error notification to MakeReservationViewModel

--- Gui/ViewModels/MakeReservationViewModel.cs
+++ Gui/ViewModels/MakeReservationViewModel.cs
@@ -2,11 +2,13 @@
 using Gui.Stores;
 using Gui.ViewModels.Abstractions;
 using Gui.ViewModels.Commands;
+using System.Collections;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace Gui.ViewModels
 {
-    public class MakeReservationViewModel : ViewModelBase
+    public class MakeReservationViewModel : ViewModelBase, INotifyDataErrorInfo
     {
         private string? _userName;
         public string UserName
@@ -55,17 +57,38 @@ namespace Gui.ViewModels
             set
             {
                 _EndDate = value;
+                _propertyNameToErrorsDictionary.Remove(nameof(EndDate));  // Clear previous errors
+                if(EndDate < StartDate)
+                {
+                    var endDateErrors = new List<string>() { 
+                        "Reservation EndDate cannot be prior than StartDate"
+                    };
+                    _propertyNameToErrorsDictionary.Add(nameof(EndDate), endDateErrors);
+                    ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(EndDate)));
+                }
                 OnPropertyChanged(nameof(EndDate));
             }
         }
         public ICommand SubmitCommand { get; }
         public ICommand CancelCommand { get; }
         public MakeReservationViewModel(HotelStore hotelStore, NavigationService navigationService)
-        {   
+        {
             SubmitCommand = new MakeReservationCommand(hotelStore, this, navigationService);
             CancelCommand = new NavigateCommand(navigationService);
+
+            _propertyNameToErrorsDictionary = new Dictionary<string, List<string>>();
             _StartDate = DateTime.Now;
             _EndDate = DateTime.Now.AddDays(1);
         }
+        #region UI Error handling
+        private Dictionary<string, List<string>> _propertyNameToErrorsDictionary;
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+        public bool HasErrors => _propertyNameToErrorsDictionary.Any();
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _propertyNameToErrorsDictionary
+                .GetValueOrDefault(propertyName, new List<string>());
+        } 
+        #endregion
     }
 }
